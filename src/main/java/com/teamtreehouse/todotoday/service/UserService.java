6f98ffb68c8c00c0837ff6c99b643861a43d612a
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-03-26 04:17:13 +0530

    add userDao while its impl is handle on its own. UserService and UserServiceImpl class use dao working. "Haven't tested the working"

--- src/main/java/com/teamtreehouse/todotoday/service/UserService.java
+++ src/main/java/com/teamtreehouse/todotoday/service/UserService.java
@@ -1,5 +1,9 @@
 package com.teamtreehouse.todotoday.service;
 
 
-public interface UserService {
+import com.teamtreehouse.todotoday.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+  User findByUsername(String username);
 }
